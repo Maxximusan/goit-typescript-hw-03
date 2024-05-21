dcf57447c78ea46c49de0ad9eebfacfde5a9fc5f
Maxximusan <103362267+Maxximusan@users.noreply.github.com>
2024-05-21 14:02:33 +0300

    hw

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,61 @@
+
+
+class Key {
+private signature: number = Math.random();
+
+getSignature(): number{
+    return this.signature
+}
+}
+
+
+class Person {
+constructor(private key: Key){
+   
+}
+
+getKey(){
+    return this.key
+    }
+
+}
+
+abstract class House {
+    protected door: Boolean ;
+    protected key: Key;
+    protected tenants: Person[] =[]
+
+    constructor(key: Key) {
+        this.door = false;
+        this.key = key;
+      }
+
+    comeIn(person: Person): void{
+        if(this.door === true){
+this.tenants.push(person)
+        } else {
+            console.log('You are not a need person');
+          }
+    }
+
+    abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+openDoor(key: Key): void{
+if(key.getSignature() === this.key.getSignature()){
+  this.door = true;
+}else {
+    console.log('Go away, i am calling the police');
+    
+    }
+} 
+}
+
+
+
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
